345ae46aaf7b0c2f4d187a96b16ca69bad2bcd1b
sea <artur.semenenko@nag.ch>
2022-06-10 13:49:47 +0200

    GUI is finished

--- src/main/java/ch/nag/tamenkessho/server/ServerApplication.java
+++ src/main/java/ch/nag/tamenkessho/server/ServerApplication.java
@@ -9,28 +9,3 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 }
-
-//class User {
-//    String email, password, token;
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public User(String email, String password) {
-//        this.email = email;
-//        this.password = password;
-//    }
-//}
